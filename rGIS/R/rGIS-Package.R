3fb4586c0333412b87267d9bb1c8a98e4cf74f66
Abraham B <baldenegro40@gmail.com>
2016-12-04 15:37:38 -0800

    Fix Description

--- rGIS/R/rGIS-Package.R
+++ rGIS/R/rGIS-Package.R
@@ -4,7 +4,7 @@
 #' @param Data a dataframe that contains addresses. It may also contain additional columns.
 #' @param AddressVar variable that contains addresses
 #' @param api_key Google's geocode API key
-#' @export datafile with coordinates
+#' @export
 #' @examples
 #' function(AddressDF, Addresses, "XXX")
 
@@ -67,3 +67,4 @@ GeoCoder <- function(Data, AddressVar, api_key) {
   return(Output)
   #function ends  
 }
+document()
