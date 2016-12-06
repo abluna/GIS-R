e9b84871d20406458b1c6351e871c65ba94735be
Abraham B <baldenegro40@gmail.com>
2016-12-05 21:25:29 -0800

    Add geosearch function and fix parameter information.

--- rGIS/R/rGIS-Package.R
+++ rGIS/R/rGIS-Package.R
@@ -1,12 +1,12 @@
-#' GeoCoder
+#' GeoCoder (using Google maps)
 #'
-#' This function allows you to enter a list of Addresses and use Google maps to locate the coordinate (WGS) of each address. This function requires a valid Google Geocoding API key.
+#' This function allows user to enter a list of Addresses and use Google maps to locate the coordinate (WGS) of each address. This function requires a valid Google Geocoding API key.
 #' @param Data a dataframe that contains addresses. It may also contain additional columns.
 #' @param AddressVar variable that contains addresses
 #' @param api_key Google's geocode API key
 #' @export
 #' @examples
-#' function(AddressDF, Addresses, "XXX")
+#' GeoCoder(AddressDF, Addresses, "XXX")
 
 GeoCoder <- function(Data, AddressVar, api_key) {
   message("Initializing Geocoding")
@@ -68,3 +68,67 @@ GeoCoder <- function(Data, AddressVar, api_key) {
   #function ends  
 }
 
+
+#' GeoSearch (using Google maps)
+#'
+#' This function allows user to enter a keyword that is searched through a set of points, each of which searches within a 50 mile radius.
+#' This function requires a valid Google Geocoding API key.
+#' @param Keyword A string. Keyword to search.
+#' @param Fishpoints A two column dataframe where column POINT_X contains X coordinates and POINT_Y contains Y coordinates. 
+#' Fishpoints should cover entire area of interest.
+#' @param APIkey A string. Google's geocode API key
+#' @export
+#' @examples
+#' GeoSearch(Keyword = "walmart", Fishpoints, APIkey = "XXX")
+
+GeoSearch <- function(Keyword, Fishpoints, APIkey) {
+  
+  #bring in libraries
+  library(jsonlite)
+  library(RCurl)
+  
+  #convert Keyword to searchable text
+  KeywordSearch <- gsub(" " ,"+",Keyword)
+  
+  #loop through each fishpoint
+  ResultsTable <- as.data.frame(matrix(nrow=0, ncol=6))
+  
+  for (i in 1:dim(Fishpoints)[1]) {
+    geodatatest1 <- getURL(paste("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=",
+                                 as.character(Fishpoints[i,"POINT_Y"]),",",as.character(Fishpoints[i,"POINT_X"]),
+                                 "&radius=50000&keyword=",as.character(KeywordSearch),"&key=",as.character(APIkey), sep=""))
+    geo_data <- fromJSON(geodatatest1)
+    
+    geo_data$results$name <- cat(gsub("\\\"" ,"\"",geo_data$results$name))
+    
+    Name <- geo_data$results$name
+    Location <- geo_data$results$geometry$location
+    Ratings <- geo_data$results$rating
+    Accuracy <- geo_data$status
+    Vicinity <- geo_data$results$vicinity
+    
+    Name[is.null(Name)] <- 0
+    Location[is.null(Location)] <- 0
+    Ratings[is.null(Ratings)] <- 0
+    Accuracy[is.null(Accuracy)] <- 0  
+    Vicinity[is.null(Vicinity)] <- 0
+    
+    
+    Geocoded_Data <- cbind(Name, Location, Ratings, Accuracy, Vicinity)
+    
+    if (Accuracy!="ZERO_RESULTS"){
+      #row.names(Geocoded_Data) <- seq(from = dim(ResultsTable)[1]+1, to = dim(Results)[1]+dim(ResultsTable)[1])
+      names(ResultsTable) <- names(Geocoded_Data)  
+      ResultsTable <- rbind(ResultsTable,Geocoded_Data)
+    }
+    
+    pb = txtProgressBar(min = 0, max = dim(Fishpoints)[1], initial = 0, style = 3)
+    setTxtProgressBar(pb,i/(100)*100)
+    
+    SearchResults <- ResultsTable
+    
+  }
+  
+  return(SearchResults)
+  
+}
